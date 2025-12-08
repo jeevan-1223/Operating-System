5ac2c6ebce730483946c825a0109d37f66da59f9
jeevan-1223 <192472080.simats@saveetha.com>
2025-12-08 14:30:33 +0530

    Add files via upload

---
+++ exp 27.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main() {
+    struct dirent *entry;
+    DIR *dp = opendir(".");
+    if (dp == NULL) return 1;
+
+    while ((entry = readdir(dp))) {
+        printf("%s\n", entry->d_name);
+    }
+    closedir(dp);
+    return 0;
+}
+
